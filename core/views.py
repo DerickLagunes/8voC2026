febdc0408238d39d85f704cb2cff6f1dfba9631b
AlexandraHernandez16 <20233tn166@utez.edu.mx>
2026-01-19 16:39:14 -0600

    [ADD] CV Katia

--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     print("alguien entro a la página principal")
     return render(request, 'core/index.html')
+
+def katia(request):
+    return render(request, 'core/katia.html')
